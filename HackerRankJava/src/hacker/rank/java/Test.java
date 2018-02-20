571a80695dad1ace0296fd9e4a7f49d69bff00d2
lothe <lothe@DESKTOP-8C69DR0>
2018-02-20 09:48:41 +0200

    Update Test.java

--- HackerRankJava/src/hacker/rank/java/Test.java
+++ HackerRankJava/src/hacker/rank/java/Test.java
@@ -8,6 +8,7 @@ import java.util.regex.*;
 
 public class Test {   
 
+	static int n = 2;
 	double mealCost = 14;
 	double tipPercent = 20;
 	double taxPercent = 8;
@@ -17,6 +18,13 @@ public class Test {
     double total = mealCost + tip + tax;
     int totalCost = (int) Math.round(total);
     
+    public static void multiply() {
+        
+    }
+    }
+
+
+    
     public Test() {
     	
     }
@@ -25,7 +33,7 @@ public class Test {
 
         Test food = new Test();
         System.out.println(food.totalCost);
-
+        multiply();
 
     }
 }
